33f2e67fec128ff701786649ebc3f88020efda98
FeryaelJustice <nano9gs@hotmail.es>
2025-02-26 20:42:21 +0100

    cors

--- server.js
+++ server.js
@@ -7,7 +7,11 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({
+    origin: "*",
+    methods: ["OPTIONS", "HEAD", "GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}));
 app.use(morgan("dev"));
 
 // Servir archivos estáticos
